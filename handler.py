4d7f565428b8c2785122adbcd37ec3b6730888f3
glennbech <glennbech@Glenns-MacBook-Pro-3.local>
2022-04-15 09:02:42 +0200

    wip

--- handler.py
+++ handler.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 
-def hello(event, context):
+def handler_main(event, context):
 
     client = boto3.client('comprehend')
     print("Received event: " + json.dumps(event, indent=2))
@@ -9,10 +9,4 @@ def hello(event, context):
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body["text"])
     return json.dumps(sentiment)
 
-if __name__ == '__main__':
 
-    event = {"body": {
-        "text": "The laptop would not boot up when I got it. It would let me get through a few steps of the setup process, then it would become unresponsive and eventually shut down, then restart, then freeze, then repeat, etc. etc. I talked to Apple Support and tried a few different restarts (NVRAM, SMC?), and tried re-installing the OS. None of that worked, so Apple Support concluded that there was probably a hardware issue. Very disappointing. I love Amazon, but they need to do better than this with their renewed electronics. I was so disappointed and frustrated with Amazon that I returned the laptop, and instead of just ordering a replacement from Amazon, I paid a few hundred extra for the same refurbished computer from Apple. I just don't have much confidence in Amazon's renewed program after this experience. I will say, however, that I'm very pleased with Amazon's return policy/process."
-    }}
-
-    context = {}
