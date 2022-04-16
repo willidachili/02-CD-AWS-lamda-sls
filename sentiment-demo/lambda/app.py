a95f561e2cca16ec277ce435e1f2ab5a34ea851f
glennbech <glennbech@glenns-mbp-3.lan>
2022-04-17 00:50:51 +0200

    print dbug

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -6,6 +6,7 @@ def handler(event, context):
     client = boto3.client('comprehend')
     print("Received event: " + json.dumps(event, indent=2))
     body = event["body"]
+    print ("Sentiment analysis on text", body["text"])
     sentiment = client.detect_sentiment(LanguageCode = "en", Text = body["text"])
     return json.dumps(sentiment)
 
