8fe79352ba34f3a351dd99d36dbe4497d368f1b9
glennbech <xp8fdael1>
2022-04-17 11:39:10 +0200

    wip

--- sentiment-demo/lambda/app.py
+++ sentiment-demo/lambda/app.py
@@ -1,20 +1,40 @@
 import json
-import boto3
+import boto
+from UnleashClient import UnleashClient
 
 def handler(event, context):
 
+# Initialize the Unleash client and the boto3 client to interact with
+# AWS and the Unleash feature toggle service.
+
+    client = UnleashClient(
+        url="https://eu.app.unleash-hosted.com/eubb1043/api/",
+        app_name="sentiment",
+        custom_headers={'Authorization': os.environ['UNLEASH_TOKEN']})
+    client.initialize_client()
     client = boto3.client('comprehend')
-    print("Received event: " + json.dumps(event, indent=2))
+
+## Process sentiment analysis onlyif the MOCK toggle is ON. Mock returns very Positive sentiment just in case
     body = event["body"]
-    print ("Sentiment analysis on text", body)
-    sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
+    if client.is_enabled("MOCK"):
+        sentiment = {
+            "Sentiment": "POSITIVE",
+            "SentimentScore": {
+                "Positive": 0.9974453449249268,
+                "Negative": 0.0074453449249268,
+                "Neutral": 0.00039782875683158636,
+                "Mixed": 0.0019206495489925146
+            }
+        }
+    else:
+        sentiment = client.detect_sentiment(LanguageCode = "en", Text = body)
 
     return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": json.dumps({
-            "sentiment ": json.dumps(sentiment)
-        })
-    }
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({
+                "sentiment ": json.dumps(sentiment)
+            })
+        }
